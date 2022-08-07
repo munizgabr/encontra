b4c76159418f1e0ca4bd92abb62fdd1aeca0ba47
munizgabr <gabrielrrtolkien@gmail.com>
2022-08-07 16:08:30 -0300

    Create the header and section search

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,13 @@ const plugin = require("tailwindcss/plugin")
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  mode: 'jit',
-  purge:{
-    enabled: true,
-    content: ['./resources/views/**/*/.blande.php', './safelist.txt'],
-    options: {
-      safelist: []
-    }
-  },
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       'header':['font-bb']
     },
     backgroundImage:{
-      'topo': "url('../src/images/pool.jpg')"
+      'topo': "url('../src/images/beachbg.jpg')"
   }
   },
   plugins: [],
