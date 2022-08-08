5cbb98cb1b6f95da6f63f353d85278643b2c3e5f
munizgabr <gabrielrrtolkien@gmail.com>
2022-08-08 12:20:24 -0300

    Add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@ const plugin = require("tailwindcss/plugin")
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      'header':['font-bb']
+    darkMode: 'class',
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            'header': ['font-bb']
+        },
+        backgroundImage: {
+            'topo': "url('../src/images/beachbg.jpg')"
+        }
     },
-    backgroundImage:{
-      'topo': "url('../src/images/beachbg.jpg')"
-  }
-  },
-  plugins: [],
+    plugins: [],
 }
